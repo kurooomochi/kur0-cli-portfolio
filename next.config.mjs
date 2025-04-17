14246127e48e8fdd0983c67205d44aa796d981c5
kur0byte <santiagozapatabohorquez@hotmail.com>
2025-04-17 00:05:43 -0500

    refactor: comment out unused configuration options in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
+  // images: {
+  //   unoptimized: true,
+  // },
+  // experimental: {
+  //   webpackBuildWorker: true,
+  //   parallelServerBuildTraces: true,
+  //   parallelServerCompiles: true,
+  // },
+  // output: 'standalone',
 }
-
 export default nextConfig
