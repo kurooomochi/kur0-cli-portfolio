11468606abb1f5a3da6775f8b25a0f06c142b824
kur0byte <santiagozapatabohorquez@hotmail.com>
2025-04-17 00:08:41 -0500

    refactor: uncomment output configuration in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,6 @@ const nextConfig = {
   //   parallelServerBuildTraces: true,
   //   parallelServerCompiles: true,
   // },
-  // output: 'standalone',
+  output: 'standalone',
 }
 export default nextConfig
